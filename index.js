da0de1673a0af4ed6756ecb5cc386fb868ff2170
Joan in DC <joanrigdon@gmail.com>
2019-01-15 21:57:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(nested,target) {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('[attr$=ranked] li');
+  
+  for (let i=0; i<lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + (parseInt(n))).toString();
+  }
+}
+
+function deepestChild(element) {
+  var nodes = document.querySelectorAll('#grand-node div');
+  return nodes[nodes.length-1]
+}
 
