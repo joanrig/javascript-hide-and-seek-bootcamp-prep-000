733ad0b14193b32da46588d9f4302fbc2c143807
Joan in DC <joanrigdon@gmail.com>
2019-01-15 23:36:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('[attr$=ranked] li');
   
   for (let i=0; i<lis.length; i++) {
-    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + (parseInt(n))).toString();
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML)) + (parseInt(n)).toString();
   }
 }
 
